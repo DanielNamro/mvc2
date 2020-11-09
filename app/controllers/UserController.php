90c19199f856ef103a09695f8b0ec4c5794df6d1
DanielNamro <danielnamrofpb@gmail.com>
2020-11-09 17:45:04 +0000

    mvc2

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,22 +1,36 @@
 <?php
 namespace App\Controllers;
 
+require_once('../app/models/User.php');
+use \App\Models\User;
+
 class UserController  
 {
     public function __construct()
     {
-        echo "en UserController<br>";
+        // echo "en UserController<br>";
     }
 
     public function index()
     {
-        echo "En método index<br>";
+        // echo "En método index<br>";
+
+        //buscar la lista de usuarios
+        $users = User::all(); //arriba pongo use ...
+        // $users = \App\Models\User::all();
+        // echo "<pre>";
+        // print_r($users);
+        //generar la vista
+        include('../views/user/index.php');
     }
     
     public function show($arguments)
     {
         $id = $arguments[0];
         echo "Mostrar el usuario $id";        
+        $user = User::find($id);
+        //generar la vista
+        include('../views/user/show.php');
     }
     
     public function delete($arguments)
---
+++ app/models/User.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Models;
+
+use PDO;
+use PDOException;
+
+class User
+{
+    public function __construct()
+    {
+        # code...
+    }
+
+    public static function all()
+    {
+        $db = User::db();
+        $statement = $db->query('SELECT * FROM users');
+        $users = $statement->fetchAll(PDO::FETCH_CLASS, User::class);
+
+        return $users;        
+    }
+
+    public static function find($id)
+    {
+        $db = User::db();
+
+        $statement = $db->prepare('SELECT * FROM users WHERE id=:id');
+        $statement->execute(array(':id' => $id));        
+        $statement->setFetchMode(PDO::FETCH_CLASS, User::class);
+        $user = $statement->fetch(PDO::FETCH_CLASS);
+        return $user;
+    }
+
+    protected static function db()
+    {
+        $dsn = 'mysql:dbname=mvc;host=db';
+        $usuario = 'root';
+        $contraseña = 'password';
+        try {
+            $db = new PDO($dsn, $usuario, $contraseña);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Falló la conexión: ' . $e->getMessage();
+        }
+        return $db;
+    }
+}
